315fb277c795cd9bb8b27953bab9c9be14ced0af
ezik_lovik <149524218+DashulikGorbunova@users.noreply.github.com>
2025-04-21 10:57:45 +0300

    Add files via upload

---
+++ с+/scr/timerange.cpp
@@ -0,0 +1,25 @@
+#include "/home/dasha/Рабочий стол/c+/include/timerange.hpp"
+#include "/home/dasha/Рабочий стол/c+/include/timedate.hpp"
+#include "timedate.cpp"
+#include <iostream>
+TimeRange::TimeRange(DateTime start, DateTime finish) {
+    this->start = start;
+    this->finish = finish;
+}
+
+bool TimeRange::overlaps(TimeRange other) {
+    if (other.start.getSeconds() > this->finish.getSeconds()) {
+        return false;
+    }
+    else if (other.finish.getSeconds() < this->start.getSeconds()) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+std::string TimeRange::toString() {
+    std::string res = "Начало промежутка: " + start.toString() + ";\nКонец промежутка: " + finish.toString();
+    return res;
+}
